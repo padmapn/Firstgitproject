3436d6256455721a783fefe5d7ee3b416f3c6571
padma <padma.pn@mialotech.com>
2020-07-19 18:41:20 +0530

    first commit on git

---
+++ Project01/src/main/java/com/selenium/Project01/App.java
@@ -0,0 +1,17 @@
+package com.selenium.Project01;
+
+
+import java.awt.Robot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+    	Robot robot = new Robot();
+        System.out.println( "Hello World!" );
+    }
+}
